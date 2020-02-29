b0199f4d0bde2f37c46b718dfe0714bc72e26c51
Linh Tito <nguyenlethanhlinh95@gmail.com>
2020-02-29 12:37:27 +0700

    Setup default

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Product from './components/product';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header></Header>
-      </header>
-      <div className="container">
-        <div className="row">
-          <Product></Product>
-        </div>
-      </div>
+      Hello
     </div>
   );
 }
