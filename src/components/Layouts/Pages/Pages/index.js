ab0df63efe47aceef236585f3136bcd3b6d778cd
Linh Tito <nguyenlethanhlinh95@gmail.com>
2020-02-29 10:21:48 +0700

    Update bootstrap cdn for idex

---
+++ src/components/Layouts/Pages/Pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            Basic panel example
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
