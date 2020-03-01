dec72df960c23f62882a6a7161ca36997640d7e6
Linh Tito <nguyenlethanhlinh95@gmail.com>
2020-03-01 18:10:13 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
+    render() {
+      var oldState = {
+        num: [1,2,3],
+        status : true
+      }
+      var redux = require('redux');
+      var reducer1 = (state = oldState, action) => {
+        switch (action.type) {
+          case "ADD_NEW_ITEM":
+            return {...state, num: [...state.num, action.newItem]}
+            break;
+          case "DELETE_ITEM":
+          return {...state, num: state.num.filter((value, index) => index != action.index ) }
+          break;
+        
+          default:
+            break;
+        }
+        return state;
+      };
+
+      var store1 = redux.createStore(reducer1);
+
+      // subcrible
+      store1.subscribe(() => {
+        console.log(store1.getState());
+      });
+      
+      var action_addnum = {
+        type: "ADD_NEW_ITEM",
+        newItem: 6
+      }
+      // thuc thi
+      store1.dispatch(action_addnum)
+
+      var action_deletenum = {
+        type: "DELETE_ITEM",
+        index: 0
+      }
+      store1.dispatch(action_deletenum)
     return (
       <div>
         hello
