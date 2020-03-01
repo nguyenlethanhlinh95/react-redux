b54496fd232f35e271292bee1e1126dceb614c3a
Linh Tito <nguyenlethanhlinh95@gmail.com>
2020-03-01 16:35:05 +0700

    Add success example redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    var redux = require('redux');
+    var oldState = {
+      num: [1,2,3],
+      status : true
+    }
+    var reducer1 = (state = oldState, action) => {
+      switch (action.type) {
+        case "CHANGE_STATUS":
+          return {...state, status: ! state.status}
+          break;
+      
+        default:
+          break;
+      }
+      return state;
+    };
+
+    var store1 = redux.createStore(reducer1);
+    console.log(store1.getState());
+    // action la 1 ten
+    var action = {
+      type: "CHANGE_STATUS",  
+    }
+    // thuc thi
+    store1.dispatch(action)
+    console.log(store1.getState());
+
+    return (
+      <div>
+          test
+      </div>
+    )
+  }
 }
 
-export default App;
